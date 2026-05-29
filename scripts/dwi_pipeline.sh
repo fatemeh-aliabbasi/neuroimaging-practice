f0da8f46e787f31e888d7c5a9b4b8ef4d7463e52
KoraTMontemagno <koram.montemagno@hotmail.com>
2026-05-29 13:51:59 +0200

    change folder structure on .sh file

--- scripts/dwi_pipeline.sh
+++ scripts/dwi_pipeline.sh
@@ -18,17 +18,19 @@
 # =========================================================
 # STEP 2 — Convert DICOMs to .mif format and filesystem path to location where DICOM data are stored
 # =========================================================
-
+# Subject ID for FreeSurfers
+SUBJECTID="sub_03"
 # Filesystem path to location where DICOM data are stored
 PROJECT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-DICOMDIR="$PROJECT_DIR/subjects/sub-03/subject/dicoms"
-# Subject ID for FreeSurfer
-SUBJECTID="sub_03"
+DICOMDIR="$PROJECT_DIR/subjects/$SUBJECTID/subject/dicoms"
+DERIVATIVEDIR="$PROJECT_DIR/derivative"
+
+SUBJDER="$DERIVATIVEDIR/$SUBJECTID"
 
 # Convert DICOMs to MRtrix .mif format
-mrconvert ${DICOMDIR}/DWI_MSMT_102_AP/ dwi.mif
-mrconvert ${DICOMDIR}/DWI_b0_PA/ b0_pa.mif
-mrconvert ${DICOMDIR}/T1w/ T1w.mif
+mrconvert ${DICOMDIR}/DWI_MSMT_102_AP/ ${SUBJDER}/dwi.mif
+mrconvert ${DICOMDIR}/DWI_b0_PA/ ${SUBJDER}/b0_pa.mif
+mrconvert ${DICOMDIR}/T1w/ ${SUBJDER}/T1w.mif
 
 #To make sure that FreeSurfer is installed and configured correctly
 echo ${SUBJECTS_DIR}
@@ -43,7 +45,8 @@ mrconvert T1w.mif T1w.nii
 # =========================================================
 recon-all -s ${SUBJECTID} -i T1w.nii -all -openmp 4
 
-
+# change directory
+cd ${SUBJDER}
 
 #preprocessing
 # =========================================================
